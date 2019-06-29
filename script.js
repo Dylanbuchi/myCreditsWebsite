0d2125debd4d4f82fabc96bad35e7a8d99e31f9c
Dylan <52018183+Dylanbuchi@users.noreply.github.com>
2019-06-29 04:36:01 -0300

    my

---
+++ script.js
@@ -0,0 +1,17 @@
+var fud = prompt("Do you agree that Fuders need to go to Hell ?");
+var why = prompt("WHY??");
+
+var response = {
+    yes: true,
+    no: false
+};
+
+function fuder(yes, no) {
+    if ((response === yes) {
+        alert("YES")
+    } else {
+        alert("Wrong username or password")
+    }
+}
+
+function fuder(fud, why);
